4f6928039eba4b61bfc6e6080258baf04b9211fb
GuiSantosDev <guisantos.dev@outlook.com>
2021-06-01 13:59:01 -0600

    cleaning up
    removed minor features and added package name option in config.json

--- index.js
+++ index.js
@@ -1,48 +1,38 @@
-/* eslint-disable no-undef */
-const prefix = require("./config.json").prefix;
-const whatsapp = require("@open-wa/wa-automate");
-const fs = require("fs");
+const wa = require("@open-wa/wa-automate")
+const { decryptMedia } = require("@open-wa/wa-decrypt");
+const config = require("./config.json")
 
-const availableCommands = new Set();
+const uaOverride = "WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
 
-fs.readdir("./commands", (e, files) => {
-    if (e) return console.error(e);
-    files.forEach(commandFile => {
-        availableCommands.add(commandFile.replace(".js", ""));
-    });
-});
+wa.create().then(client => start(client))
 
-whatsapp.create({
-    useChrome: true,
-    headless: true,
-    chromiumArgs: ["--no-sandbox", "--disable-setuid-sandbox"]
-}).then(bot => start(bot));
-
-let args;
-let command;
-
-function start(bot) {
-    bot.onStateChanged(async state => {
-        console.log("[Client State]", state);
-        if (state === "CONFLICT" || state === "UNLAUNCHED") bot.forceRefocus();
-    });
-
-    bot.onMessage(async message => {
-        try {
-            if (message.body.startsWith(prefix)) {
-                args = message.body.slice(prefix.length).trim().split(/ +/g);
-                command = args.shift().toLowerCase();
-                sender = message.sender.pushname;
-            } else if (message.caption.startsWith(prefix)) {
-                args = message.caption.slice(prefix.length).trim().split(/ +/g);
-                command = args.shift().toLowerCase();
-                sender = message.sender.pushname;
-            } else {
-                return;
-            }
-        } catch { }
-        if (availableCommands.has(command)) {
-            require(`./commands/${command}`).run(bot, message, args);
+async function start(client) {
+    client.onMessage(async (message) => {
+        const now = Date.now();
+        if (message.caption === `${config.prefix}sticker` && message.isMedia && message.type === "image") {
+            const media = await decryptMedia(message, uaOverride);
+            await client.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`, { author: `Solicitado por: ${message.sender.pushname}`, pack: config.packageName });
+            console.log(`[DEBUG] Sticker was generated in ${Date.now() - now}ms`);
         }
-    });
+            else if (message.body === `${config.prefix}sticker` && message.quotedMsgObj && message.quotedMsgObj.type === "image") {
+            const media = await decryptMedia(message.quotedMsgObj, uaOverride);
+            await client.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`, { author: `Solicitado por: ${message.sender.pushname}`, pack: config.packageName });
+            console.log(`[DEBUG] Sticker was generated in ${Date.now() - now}ms`);
+        }
+            else if (message.caption === `${config.prefix}sticker` && message.isMedia) {
+                if (message.mimetype === "video/mp4" || message.mimetype === "image/gif" || message.type === "video") {
+                    if (message.duration >= 10) return client.reply(message.from, "Vídeo muito longo, tamanho máximo: 10 segundos", message.id); {
+                        client.reply(message.from, "_Espera ai amor..._", message.id);
+                        const mediaData = await decryptMedia(message, uaOverride);
+                        try {
+                            await client.sendMp4AsSticker(message.from, mediaData, { }, { author: `Solicitado por: ${message.sender.pushname}`, pack: config.packageName })
+                            console.log(`[DEBUG] Sticker was generated in ${Date.now() - now}ms`)
+                        } catch (error) {
+                            console.log(message.from, "Erro", message.id);
+                        }
+                    }
+                }
+            }
+        }
+    )
 }

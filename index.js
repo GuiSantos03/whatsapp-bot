0d9a3742bbbf7c058380f43811f59cebda5570dd
Guilherme Santos <guisantos.dev@outlook.com>
2021-06-16 16:15:05 -0300

    refactored

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const config = require("./config.json")
 wa.create().then(client => start(client))
 const consign = Consign()
 
+consign
+	.include("commands")
+	.exclude("commands/libs")
+
 async function start(client) {
-    client.onMessage(async (message) => {
-        const now = Date.now()     
+	client.onMessage(async (message) => {
+		const now = Date.now()     
 
-        consign
-            .include("commands")
-            .into(client, message, now, config)
+		consign
+			.into(client, message, now, config)		
 
-    })
+	})
 }

fcaa36bd58f38ec76cbdf20b4940f259436d9f5a
Guilherme Santos <guisantos.dev@outlook.com>
2021-06-16 16:14:40 -0300

    refactored

--- commands/sticker/sticker.js
+++ commands/sticker/sticker.js
@@ -1,10 +1,9 @@
 const { decryptMedia } = require("@open-wa/wa-decrypt")
 
-
 module.exports = async (client, message, now, config) => {
-    if (message.caption === `${config.prefix}sticker` && message.isMedia && message.type === "image") {
-        const media = await decryptMedia(message, config.userAgent)
-        await client.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`, { author: `Solicitado por: ${message.sender.pushname}`, pack: config.packageName });
-        console.log(`[DEBUG] Sticker gerado em ${Date.now() - now}ms`);
-    }
+	if (message.caption === `${config.prefix}sticker` && message.isMedia && message.type === "image") {
+		const media = await decryptMedia(message, config.userAgent)
+		await client.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`, { author: `Solicitado por: ${message.sender.pushname}`, pack: config.packageName })
+		console.log("\x1b[32m",`[DEBUG] Sticker gerado em ${Date.now() - now}ms`)
+	}
 }

ae75c3439b87a65db61270cb1d7be36e6069b9b2
Guilherme Santos <guisantos.dev@outlook.com>
2021-06-16 16:12:32 -0300

    extracted the submission into a module

--- commands/sticker/animatedSticker.js
+++ commands/sticker/animatedSticker.js
@@ -1,25 +1,9 @@
-const { decryptMedia } = require("@open-wa/wa-decrypt")
+const sendAnimatedSticker = require("../libs/sendAnimatedSticker")
 
 module.exports = async function(client, message, now, config) {
-    if (message.caption === `${config.prefix}sticker` && message.isMedia) {
-        if (message.mimetype === "video/mp4" || message.mimetype === "image/gif" || message.type === "video") {
-            animatedMediaToSticker(client, message, now)
-        }
-    }
-
-    async function animatedMediaToSticker(client, message, now) {
-        if (message.duration >= 10) {
-            return client.reply(message.from, "Tamanho máximo: 10 segundos", message.id)  
-        }
-
-        client.reply(message.from, "_Gerando..._", message.id)
-        const mediaData = await decryptMedia(message, config.userAgent)
-
-        try {
-            await client.sendMp4AsSticker(message.from, mediaData, { }, { author: `Solicitado por: ${message.sender.pushname}`, pack: config.packageName })
-            console.log(`[DEBUG] Sticker gerado em ${Date.now() - now}ms`)
-        } catch (error) {
-            console.log(message.from, "Erro", message.id)
-        }
-    }
+	if (message.caption === `${config.prefix}sticker` && message.isMedia) {
+		if (message.mimetype === "video/mp4" || message.mimetype === "image/gif" || message.type === "video") {
+			sendAnimatedSticker(client, message, config, now)
+		}
+	}
 }
--- commands/sticker/quotedSticker.js
+++ commands/sticker/quotedSticker.js
@@ -1,27 +1,16 @@
 const { decryptMedia } = require("@open-wa/wa-decrypt")
+const sendAnimatedSticker = require("../libs/sendAnimatedSticker")
 
 module.exports = async function(client, message, now, config) {
-    if (message.body === `${config.prefix}sticker` && message.quotedMsgObj) {
-        if (message.quotedMsgObj.type === "image") {
-            const media = await decryptMedia(message.quotedMsgObj, config.userAgent)
-            await client.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`, { author: `Solicitado por: ${message.sender.pushname}`, pack: config.packageName })
-            console.log(`[DEBUG] Sticker gerado em ${Date.now() - now}ms`)
-        } 
+	if (message.body === `${config.prefix}sticker` && message.quotedMsgObj) {
+		if (message.quotedMsgObj.type === "image") {
+			const media = await decryptMedia(message.quotedMsgObj, config.userAgent)
+			await client.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`, { author: `Solicitado por: ${message.sender.pushname}`, pack: config.packageName })
+			console.log("\x1b[32m",`[DEBUG] Sticker gerado em ${Date.now() - now}ms`)
+		} 
 
-        else if (message.quotedMsgObj.type === "video/mp4" || message.quotedMsgObj.type === "image/gif" || message.quotedMsgObj.type === "video") {
-
-            if (message.quotedMsgObj.duration >= 10) {
-                return client.reply(message.from, "Tamanho máximo: 10 segundos", message.id)  
-            }
-
-            client.reply(message.from, "_Gerando..._", message.id)
-            const mediaData = await decryptMedia(message.quotedMsgObj, config.userAgent)
-            try {
-                await client.sendMp4AsSticker(message.from, mediaData, { }, { author: `Solicitado por: ${message.sender.pushname}`, pack: config.packageName })
-                console.log(`[DEBUG] Sticker gerado em ${Date.now() - now}ms`)
-            } catch (error) {
-                console.log(message.from, "Erro", message.id)
-            }
-        }
-    }
+		else if (message.quotedMsgObj.type === "video/mp4" || message.quotedMsgObj.type === "image/gif" || message.quotedMsgObj.type === "video") {
+			sendAnimatedSticker(client, message, config, now)
+		}
+	}
 }

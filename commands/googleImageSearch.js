1f0a6f95adc5307dcc115cca72d6c7530aae314b
Guilherme Santos <guisantos.dev@outlook.com>
2021-06-16 16:09:07 -0300

    refactored

--- commands/googleImageSearch.js
+++ commands/googleImageSearch.js
@@ -1,4 +1,4 @@
-const GoogleImages = require('google-images')
+const GoogleImages = require("google-images")
 
 const credentials = require("../credentials.json")
 const imageSearch = new GoogleImages(credentials.searchEngineId, credentials.apiKey)
@@ -6,33 +6,36 @@ const imageSearch = new GoogleImages(credentials.searchEngineId, credentials.api
 
 
 module.exports = async (client, message, now, config) => {
-    if (message.body != undefined && message.body.indexOf(`${config.prefix}pesquisar`) != -1) {
-        try {
-            imageSearch.search(message.body.slice(11))
-                .then( async image => {
-                    try {
-                        await client.sendImage(message.from, `${image[0].url}`, "", `Resultado da pesquisa de *${message.sender.pushname}*: \n\n_${image[1].description}_`)
-                        console.log(`Imagem enviada com descrição em: ${Date.now() - now}ms`)
-                    } catch {                    
-                        if (thumbnailNotUndefined == true) {
-                            await client.sendImage(message.from, `${image[0].thumbnail.url}`, "", `Resultado da pesquisa de *${message.sender.pushname}*`)
-                            console.log(`thumbnail enviada em: ${Date.now() - now}ms`)
-                        } else {
-                            await client.reply(message.from, `Nenhum resultado encontrado para: ${message.body.slice(11)}`, message.id)
-                            console.log("Imagem não enviada")
-                        }
-                    }})
-        } catch {
-            await client.reply(message.from, `Nenhum resultado encontrado para: ${message.body.slice(11)}`, message.id)
-            console.log("Imagem não enviada")
-        }
-    }
+	if (message.body != undefined && message.body.indexOf(`${config.prefix}pesquisar`) != -1) {
+
+		imageSearch.search(message.body.slice(11))
+			.then( async image => {
+				try {
+					await client.sendImage(message.from, `${image[0].url}`, "", `Resultado da pesquisa de *${message.sender.pushname}*: \n\n_${image[1].description}_`)
+					console.log("\x1b[32m", `[DEBUG] Imagem enviada com descrição em: ${Date.now() - now}ms`)
+				} catch {                    
+					if (thumbnailNotUndefined(image)) {
+						await client.sendImage(message.from, `${image[0].thumbnail.url}`, "", `Resultado da pesquisa de *${message.sender.pushname}*`)
+						console.log("\x1b[33m",`[DEBUG] thumbnail enviada em: ${Date.now() - now}ms`)
+					} else {
+						await client.reply(message.from, `Nenhum resultado encontrado para: ${message.body.slice(11)}`, message.id)
+						console.log("\x1b[31m","[DEBUG] Imagem não econtrada")
+					}
+				}})
+			.catch(async (error) => {
+				await client.reply(message.from, `Nenhum resultado encontrado para: ${message.body.slice(11)}`, message.id)
+				console.error(error)
+				console.log("\x1b[31m","[DEBUG] Imagem não encontrada")
+					
+			})
+
+	}
 }
 
-
-thumbnailNotUndefined = function(image) {
-    if (image[0].thumbnail.url != undefined) {
-        return true
-    }
+function thumbnailNotUndefined(image) {
+	if (image[0].thumbnail.url !== undefined) {
+		return true
+	}
+	return false
 }
 

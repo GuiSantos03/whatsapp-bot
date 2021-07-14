e1bd6dbeb1c7dd217bd6c8f5da383af87c6dcb2c
GuiSantosDev <guisantos.dev@outlook.com>
2021-07-14 14:11:20 -0300

    updated menu path

--- src/commands/menu.js
+++ src/commands/menu.js
@@ -2,7 +2,7 @@ const { readFile } = require("fs");
 
 module.exports = (client, message, now, config) => {
     if (message.body !== undefined && message.body.startsWith(`${config.prefix}menu`)) {
-        readFile("./commands/libs/utils/commands.txt", "utf-8", (error, data) => {
+        readFile(`${process.cwd()}/src/commands/libs/utils/commands.txt`, "utf-8", (error, data) => {
             client.reply(message.from, data, message.id);
             console.log("\x1b[32m", `[LOG] Menu enviado em ${Date.now() - now}ms`);
             if (error) console.error(error);
